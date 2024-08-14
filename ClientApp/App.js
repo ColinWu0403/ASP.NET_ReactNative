5a426024cc6d2737d90fe0deb0b344b65dd4f773
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-08-14 10:57:49 -0400

    Configured TailwindCSS for React Native

--- ClientApp/App.js
+++ ClientApp/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+    default: "native",
+});
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex w-full align-center justify-center bg-amber-50">
+      <Text className="text-2xl font-bold text-center">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ ClientApp/postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [require("tailwindcss"), require("nativewind/postcss")],
+};
+
+module.exports = {
+  plugins: {
+    "nativewind/postcss": {
+      output: "nativewind-output.js",
+    },
+  },
+};
+
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
