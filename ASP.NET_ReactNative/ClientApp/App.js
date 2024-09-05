1bd12c6338877d4578a8e40eb20873050f799971
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-09-04 20:58:09 -0400

    Update App.js

--- ASP.NET_ReactNative/ClientApp/App.js
+++ ASP.NET_ReactNative/ClientApp/App.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
@@ -17,7 +15,7 @@ export default function App() {
       <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
-          headerShown: true, // Hide header for a cleaner UI
+          headerShown: true, // Set to false to hide header
         }}
       >
         <Stack.Screen name="Home" component={HomePage} />
